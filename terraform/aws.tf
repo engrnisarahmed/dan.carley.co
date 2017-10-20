2be2fc29bab900aad4cb853f1eaace9f829112f0
Dan Carley <dan.carley@gmail.com>
2017-10-20 11:37:01 +0100

    Use ACM cert for CloudFront
    Certificates have to be manually requested from the console, but we can use
    a data resource to lookup the ARN.
    
    The certificate has to be requested in us-east-1 and pull in using a
    separate provider alias because:
    
    > To use an ACM Certificate with Amazon CloudFront, you must request or
    > import the certificate in the US East (N. Virginia) region. ACM
    > Certificates in this region that are associated with a CloudFront
    > distribution are distributed to all the geographic locations configured
    > for that distribution.
    
    http://docs.aws.amazon.com/acm/latest/userguide/acm-regions.html

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+provider "aws" {
+  alias  = "us-east"
+  region = "us-east-1"
+}
+
 terraform {
   backend "s3" {
     bucket = "dan.carley.co-tf"
--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,3 +1,9 @@
+data "aws_acm_certificate" "website" {
+  domain   = "${var.website_name}"
+  statuses = ["ISSUED"]
+  provider = "aws.us-east"
+}
+
 resource "aws_cloudfront_distribution" "website" {
   origin {
     domain_name = "${aws_s3_bucket.website.website_endpoint}"
@@ -46,6 +52,8 @@ resource "aws_cloudfront_distribution" "website" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn      = "${data.aws_acm_certificate.website.arn}"
+    minimum_protocol_version = "TLSv1"
+    ssl_support_method       = "sni-only"
   }
 }
