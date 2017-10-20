b7d5c41a458c41fa241453d1ce0b77cd32d938de
Dan Carley <dan.carley@gmail.com>
2017-10-20 11:34:12 +0100

    Setup CloudFront
    In order to get HTTPS and IPv6. S3 website hosting doesn't provide these.

---
+++ terraform/cloudfront.tf
@@ -0,0 +1,51 @@
+resource "aws_cloudfront_distribution" "website" {
+  origin {
+    domain_name = "${aws_s3_bucket.website.website_endpoint}"
+    origin_id   = "origin-bucket-${aws_s3_bucket.website.id}"
+
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port              = "80"
+      https_port             = "443"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "${var.website_name}"
+  default_root_object = "index.html"
+
+  aliases = ["${var.website_name}"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "origin-bucket-${aws_s3_bucket.website.id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
